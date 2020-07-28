bfc0d29c47f4fe08159bc19de414ac5030d7ab59
beifa <beifabeifa@mail.ru>
2020-07-28 12:01:44 +0300

    change to size and format to jpg

--- src/dataset.py
+++ src/dataset.py
@@ -4,7 +4,7 @@ import joblib
 import numpy as np
 from torch.utils.data import Dataset
 
-PATH_PKL_128 = r'C:\Users\pka\kaggle\melanoma\input\siim-isic-melanoma-classification\pkl_128'
+PATH_PNG_224 = r'C:\Users\pka\kaggle\melanoma\input\siim-isic-melanoma-classification\png_224\train'
 
 class trainDataset(Dataset):
 
@@ -15,15 +15,21 @@ class trainDataset(Dataset):
     def __len__(self):
         return len(self.data)
 
-    def __getitem__(self, idx):       
-        name = self.data.image_name.values[idx]        
-        image = joblib.load(os.path.join(PATH_PKL_128, f'{name}.pkl'))
-        if self.transform:
-            image = self.transform(image)
+    def __getitem__(self, idx):
+       
+        name = self.data.image_name.values[idx]
+        
+       
+        image = cv2.imread(os.path.join(PATH_PNG_224, f'{name}.png'))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        if self.transform is not None:
+            image = self.transform(image=image)['image']
         image = image.astype(np.float32)
         image /= 255
-        image = np.transpose(image, (2,0,1))
-        target = self.data.target.values[idx]      
+        image = np.transpose(image, (2, 0, 1))
+        target = self.data.target.values[idx]
+      
         
         return torch.tensor(image), torch.tensor(target, dtype = torch.float)
 
